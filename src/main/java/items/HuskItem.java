0c7ee7f8ced71c8d918d106f796d90804f38c10f
Tut <tutsandallthat@gmail.com>
2017-11-06 11:24:56 -0500

    attempted to fix chest inventory interaction - done in class - not tested ( ! )

--- src/main/java/items/HuskItem.java
+++ src/main/java/items/HuskItem.java
@@ -12,17 +12,20 @@ import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
 import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.text.TextComponentString;
 import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.items.CapabilityItemHandler;
+import sun.plugin2.message.TextEventMessage;
 import utils.ChestCoordStorage;
 import utils.ModItems;
 
 import java.util.ArrayList;
 
-public class HuskItem extends Item {
+public class HuskItem extends Item
+{
 
     private ArrayList<ItemStack> newlist = new ArrayList<>();
     private ChestCoordStorage[] chestList = new ChestCoordStorage[2];
@@ -30,48 +33,58 @@ public class HuskItem extends Item {
     private int totalItemUses = 0;
     private boolean itemisUsed = false;
 
-    public HuskItem() {
+    public HuskItem()
+    {
         setRegistryName("huskitem");
         setUnlocalizedName(TutMod.MODID + ".huskitem");
 
     }
 
     @SideOnly(Side.CLIENT)
-    public void initModel(){
-        ModelLoader.setCustomModelResourceLocation(this, 0 , new ModelResourceLocation(getRegistryName(), "inventory"));
+    public void initModel()
+    {
+        ModelLoader.setCustomModelResourceLocation(this, 0, new ModelResourceLocation(getRegistryName(), "inventory"));
     }
 
     @Override
-    public EnumActionResult onItemUseFirst(EntityPlayer player, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand) {
-        if (!worldIn.isRemote) {
+    public EnumActionResult onItemUseFirst(EntityPlayer player, World worldIn, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand)
+    {
+        if (!worldIn.isRemote)
+        {
             if (player.getHeldItem(player.getActiveHand()).getItem() == ModItems.huskItem)
             {
 
                 TileEntity locatedChest = worldIn.getTileEntity(pos);
 
-                if (locatedChest instanceof TileEntityChest) {
+                if (locatedChest instanceof TileEntityChest)
+                {
+                    player.sendMessage(new TextComponentString("Chest located at: " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ()));
                     System.out.println("Chest located at: " + pos.getX() + ", " + pos.getY() + ", " + pos.getZ());
 
 
-                    if (!locatedChest.getTileData().hasKey("hasBeenAccessed") && totalItemUses <= 1) {
+                    if (!locatedChest.getTileData().hasKey("hasBeenAccessed") && totalItemUses <= 1)
+                    {
                         totalItemUses++;
 
                         chestList[totalItemUses - 1] = new ChestCoordStorage(pos.getX(), pos.getY(), pos.getZ());
 
                         locatedChest.getTileData().setBoolean("hasBeenAccessed", true);
 
-                        for (int i = 0; i < ((TileEntityChest) locatedChest).getSizeInventory(); i++) {
+                        for (int i = 0; i < ((TileEntityChest) locatedChest).getSizeInventory(); i++)
+                        {
 
                             ItemStack itemHolder = (((TileEntityChest) locatedChest).getStackInSlot(i));
 
-                            if (itemHolder.getItem() != Items.AIR) {
+                            if (itemHolder.getItem() != Items.AIR)
+                            {
                                 newlist.add(itemHolder);
                             }
                         }
                     }
 
-                    if (!locatedChest.getTileData().getBoolean("hasBeenAccessed") && itemisUsed ) {
-
+                    if (!locatedChest.getTileData().getBoolean("hasBeenAccessed") && itemisUsed)
+                    {
+                        player.sendMessage(new TextComponentString("Using item!"));
                         System.out.println("Using item!");
 
                         for (int i = 0; i < ((TileEntityChest) locatedChest).getSizeInventory(); i++)
@@ -88,10 +101,10 @@ public class HuskItem extends Item {
 
                             for (int i = 0; i < newlist.size(); i++)
                             {
-
+                                player.sendMessage(new TextComponentString(newlist.get(i).toString()));
                                 System.out.println(newlist.get(i));
 
-                                ((TileEntityChest) locatedChest).setInventorySlotContents(airList.get(i) , newlist.get(i));
+                                ((TileEntityChest) locatedChest).setInventorySlotContents(airList.get(i), newlist.get(i));
 
                                 ClearChestStorage(chestList[0].getX(), chestList[0].getY(), chestList[0].getZ(), worldIn);
                                 ClearChestStorage(chestList[1].getX(), chestList[1].getY(), chestList[1].getZ(), worldIn);
@@ -101,13 +114,15 @@ public class HuskItem extends Item {
                             newlist.clear();
                             totalItemUses = 0;
 
-                        } else {
+                        } else
+                        {
                             System.out.println("Not enough space!");
                         }
                     }
 
                     if (totalItemUses == 2)
                         itemisUsed = true;
+                    player.sendMessage(new TextComponentString(newlist.toString()));
                     System.out.println(newlist);
                 }
 
@@ -124,7 +139,7 @@ public class HuskItem extends Item {
 
         TileEntity locatedChest = worldIn.getTileEntity(chestBlock);
 
-        for(int i=0; i < ((TileEntityChest) locatedChest).getSizeInventory(); i++)
+        for (int i = 0; i < ((TileEntityChest) locatedChest).getSizeInventory(); i++)
         {
             locatedChest.getCapability(CapabilityItemHandler.ITEM_HANDLER_CAPABILITY, null).extractItem(i, ((TileEntityChest) locatedChest).getStackInSlot(i).getCount(), false);
         }
