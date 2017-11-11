cf603ffa76e5df97a07a99b134cb53766bfe8d1a
Tut <tutsandallthat@gmail.com>
2017-11-10 19:41:01 -0500

    Rebuilt HuskItem Functionality and its packet. Strange crash - investigating

--- src/main/java/items/HuskItem.java
+++ src/main/java/items/HuskItem.java
@@ -4,6 +4,8 @@ import core.TutMod;
 import net.minecraft.client.renderer.block.model.ModelResourceLocation;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.tileentity.TileEntityChest;
 import net.minecraft.util.EnumActionResult;
 import net.minecraft.util.EnumFacing;
 import net.minecraft.util.EnumHand;
@@ -12,6 +14,8 @@ import net.minecraft.world.World;
 import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
+import network.tutmodHuskItemMessage;
+import network.tutmodPacketHandler;
 
 public class HuskItem extends Item
 {
@@ -31,6 +35,77 @@ public class HuskItem extends Item
     @Override
     public EnumActionResult onItemUseFirst(EntityPlayer player, World world, BlockPos pos, EnumFacing side, float hitX, float hitY, float hitZ, EnumHand hand)
     {
+        if(world.getTileEntity(pos) instanceof TileEntityChest)
+        {
+            NBTTagCompound itemUses;
+            if(player.getHeldItem(hand).hasTagCompound())
+            {
+                itemUses = player.getHeldItem(hand).getTagCompound();
+            }
+            else
+            {
+                itemUses = new NBTTagCompound();
+            }
+
+            System.out.println(itemUses);
+
+            if(!itemUses.hasKey("Uses"))
+            {
+
+                itemUses.setInteger("Uses", 1);
+
+                int ChestCoord[] = new int[3];
+
+
+                ChestCoord[0] = pos.getX(); ChestCoord[1] = pos.getY(); ChestCoord[2] = pos.getZ();
+                itemUses.setIntArray("Chest1Coord", ChestCoord);
+
+
+            System.out.println("Chest 1 Set");
+            }
+            else if(itemUses.getInteger("Uses") == 1)
+            {
+                itemUses.setInteger("Uses", itemUses.getInteger("Uses") + 1);
+
+                int ChestCoord[] = new int[3];
+
+
+                ChestCoord[0] = pos.getX(); ChestCoord[1] = pos.getY(); ChestCoord[2] = pos.getZ();
+
+                if(ChestCoord != itemUses.getIntArray("Chest1Coord"))
+                {
+                    itemUses.setIntArray("Chest2Coord", ChestCoord);
+                }
+                else
+                System.out.println("You have selected the same chest twice. Try again.");
+
+
+                System.out.println("Chest 2 Set");
+            }
+            else if(itemUses.getInteger("Uses") == 2)
+            {
+                int ChestCoord[] = new int[3];
+
+
+                ChestCoord[0] = pos.getX(); ChestCoord[1] = pos.getY(); ChestCoord[2] = pos.getZ();
+                itemUses.setIntArray("Chest3Coord", ChestCoord);
+
+
+                System.out.println("Chest 3 Set");
+
+
+                itemUses.setInteger("Uses", itemUses.getInteger("Uses") + 1);
+            }
+
+            player.getHeldItem(hand).setTagCompound(itemUses);
+            System.out.println(player.getHeldItem(hand).getTagCompound().getInteger("Uses"));
+        }
+
+        if(world.isRemote && player.getHeldItem(hand).hasTagCompound() && player.getHeldItem(hand).getTagCompound().getInteger("Uses") == 3)
+        {
+                System.out.println("Sending data to server..");
+                tutmodPacketHandler.INSTANCE.sendToServer(new tutmodHuskItemMessage(player.getHeldItem(hand).getTagCompound().getIntArray("Chest1Coord"), player.getHeldItem(hand).getTagCompound().getIntArray("Chest2Coord"), player.getHeldItem(hand).getTagCompound().getIntArray("Chest3Coord")));
+        }
         return super.onItemUseFirst(player, world, pos, side, hitX, hitY, hitZ, hand);
     }
 }
--- src/main/java/network/tutmodHuskItemMessage.java
+++ src/main/java/network/tutmodHuskItemMessage.java
@@ -2,31 +2,75 @@ package network;
 
 import io.netty.buffer.ByteBuf;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.item.ItemStack;
+import net.minecraft.tileentity.TileEntity;
+import net.minecraft.tileentity.TileEntityChest;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessage;
 import net.minecraftforge.fml.common.network.simpleimpl.IMessageHandler;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
+import java.util.ArrayList;
+
+
 public class tutmodHuskItemMessage implements IMessage
 {
+    private int[][] chestCoords;
 
     public tutmodHuskItemMessage()
     {
     }
 
-    private void testClass(int size, int size1, int size2)
+    public tutmodHuskItemMessage(int[] chest1Coords, int[] chest2Coords, int[] chest3Coords)
     {
+        chestCoords = new int[3][3];
+        chestCoords[0] = chest1Coords;
+        chestCoords[1] = chest2Coords;
+        chestCoords[2] = chest3Coords;
     }
 
     @Override
     public void fromBytes(ByteBuf buf)
     {
+        chestCoords = new int[3][3];
+
+        for(int x = 0; x < 3; x++)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                chestCoords[x][i] = buf.readInt();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                chestCoords[x][i] = buf.readInt();
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                chestCoords[x][i] = buf.readInt();
+            }
+        }
     }
 
     @Override
     public void toBytes(ByteBuf buf)
     {
+        for(int i = 0; i < 3; i++)
+        {
+            for (int x = 0; i < 3; i++)
+            {
+                buf.writeInt(chestCoords[x][i]);
+            }
+            for (int x = 0; i < 3; i++)
+            {
+                buf.writeInt(chestCoords[x][i]);
+            }
+            for (int x = 0; i < 3; i++)
+            {
+                buf.writeInt(chestCoords[x][i]);
+            }
+        }
     }
 
     public static class Handler implements IMessageHandler<tutmodHuskItemMessage, IMessage>
@@ -40,8 +84,53 @@ public class tutmodHuskItemMessage implements IMessage
 
         private void handle(tutmodHuskItemMessage message, MessageContext ctx)
         {
+            TileEntity chestList[] = new TileEntity[3];
             EntityPlayerMP EntityPlayer = ctx.getServerHandler().player;
             World world = EntityPlayer.getEntityWorld();
+
+            for (int i = 0; i > 3; i++)
+            {
+                chestList[i] = world.getTileEntity(new BlockPos(message.chestCoords[i][0], message.chestCoords[i][1], message.chestCoords[i][2]));
+            }
+
+            ArrayList<ItemStack> itemsList = new ArrayList<>();
+            ArrayList<Integer> totalEmptySlots = new ArrayList<>();
+
+            for (int i = 0; i > 3; i++)
+            {
+                if(i <= 2)
+                {
+                    for( int x = 0; x < ((TileEntityChest) chestList[i]).getSizeInventory(); x++)
+                    {
+                        if( ((TileEntityChest) chestList[i]).getStackInSlot(x) == null)
+                        {
+                            totalEmptySlots.add(x);
+                        }
+                    }
+
+                    if(totalEmptySlots.size() >= itemsList.size())
+                    {
+                        for(int x = 0; x < itemsList.size(); x++)
+                        {
+                            ((TileEntityChest) chestList[i]).setInventorySlotContents(totalEmptySlots.get(x), itemsList.get(x));
+                        }
+                    }
+                    else
+                    {
+                        System.out.println("Not enough space in chest!");
+                    }
+                }
+                else
+                {
+                    for( int x = 0; x < ((TileEntityChest) chestList[i]).getSizeInventory(); x++)
+                    {
+                        if(((TileEntityChest) chestList[i]).getStackInSlot(x) != null)
+                        {
+                            itemsList.add(((TileEntityChest) chestList[i]).getStackInSlot(x));
+                        }
+                    }
+                }
+            }
         }
     }
 }
